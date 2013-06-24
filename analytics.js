502a5a9f27451af11c81a6274a8d46c51c4bc5fd
James Burns <jameburn@gmail.com>
2013-06-24 12:13:11 -0400

    add switch to event logging

--- analytics.js
+++ analytics.js
@@ -8,6 +8,8 @@ function( app ) {
 
     return Backbone.Model.extend({
 
+        loggingEnabled: trues,
+
         initialize: function() {
             app.on( "all", this.onEvent, this );
             if( !window.mixpanel ){
@@ -21,7 +23,7 @@ function( app ) {
             } else if( _.contains( this.modelEvents, event ) ) {
                 this.parseModelEvent (event, args);
             } else {
-                console.log("untracked event:: ", event, args );
+                //console.log("untracked event:: ", event, args );
             }
         },
 
@@ -34,16 +36,30 @@ function( app ) {
                     type: model.get("type"),
                     source: model.get("attr").archive ?  model.get("attr").archive : "none"
                 }
+            } else if ( model.modelType == "sequence" ){
+                params = {
+                    pageCount: model.frames.length
+                }
+            } else if ( model.modelType == "item" ){
+                params = {
+                    type: model.get("media_type"),
+                    source: model.get("archive") ?  model.get("archive") : "none"
+                }
             }
             
+            params = _.extend( params, model.eventData );
+
             this.trackEvent( event, params );
 
 
         },
 
         setGlobals: function ( args ){
+
             _.each(args, function (value, key){
-                mixpanel.register({ key : value } );
+                var param = {};
+                param[ key ] = value;
+                mixpanel.register( param );
             });
         },
 
@@ -57,10 +73,16 @@ function( app ) {
             "media_search",
             "page_added",
             "share",
-            "view_item",
+           // "view_item",
             "layer_font_change",
             "toggle_help",
-            "help"
+            "help",
+            "preview_toggle_view",
+            "toggle_page_background",
+            "new_zeega",
+            "to_profile",
+            "to_home",
+            "advance_toggle"
         ],
 
         modelEvents: [
@@ -70,19 +92,30 @@ function( app ) {
             "layer_deleted",
             "soundtrack_added_success",
             "soundtrack_delete",
+            "pages_reordered",
+            "layers_reordered",
+            "select_link_page",
+            "link_new_page",
+            "unlink",
+            "init_link"
 
 
         ],
 
         generateConsole: function(){
+
+            var debug = this.loggingEnabled;
+
             window.mixpanel = {
                 register: function (obj){
-                    _.each(obj, function( value, key ){
-                        console.log("registering global property::  " + key + " : " + value );
-                    });
-                },
+                    if( debug ){
+                        console.log("registering global property::  " + _.keys(obj) + " : " + _.values(obj) );
+                    }
+                },                
                 track: function ( event, params ){
-                    console.log( "tracking event:: " + event, params );
+                    if( debug ){
+                        console.log( "tracking event:: " + event, params );
+                    }
                 }
             }
         }
